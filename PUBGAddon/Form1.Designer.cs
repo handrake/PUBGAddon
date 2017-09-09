de7f092a1d0fdc360bdfac9cd84689040a38010a
Byoung Gi Lee <handrake@gmail.com>
2017-09-10 02:12:19 +0900

    initial commit

---
+++ PUBGAddon/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace PUBGAddon
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(150, 110);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(130, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "서버 위치 :";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(300, 105);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(170, 35);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(154, 220);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(316, 50);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "서버 검색";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(150, 160);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(130, 24);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "IP 주소 :";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(300, 155);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(170, 35);
+            this.textBox2.TabIndex = 4;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(80, 50);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(430, 32);
+            this.comboBox1.TabIndex = 5;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(574, 329);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "배그 서버 위치 검색기";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ PUBGAddon/Form1.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using PcapDotNet.Core;
+using PcapDotNet.Packets;
+using PcapDotNet.Packets.IpV4;
+using System.Net;
+using System.Net.Sockets;
+using System.Configuration;
+
+namespace PUBGAddon
+{
+    public partial class Form1 : Form
+    {
+        private PacketDevice selectedDevice;
+        private IPAddress localIP;
+        private IList<Tuple<String, String>> japanServerList;
+        private IList<Tuple<String, String>> koreaServerList;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            IList<LivePacketDevice> allDevices = LivePacketDevice.AllLocalMachine;
+            
+            if (allDevices.Count == 0)
+            {
+                MessageBox.Show("네트워크 장치가 없습니다.", "오류", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Application.Exit();
+            }
+
+            for (int i = 0; i < allDevices.Count(); i++)
+            {
+                comboBox1.Items.Add(allDevices[i].Description);
+            }
+
+            comboBox1.SelectedIndex = 0;
+
+            String[] tmpList1 = ConfigurationManager.AppSettings["KoreaServerList"].Split('|');
+            String[] tmpList2 = ConfigurationManager.AppSettings["JapanServerList"].Split('|');
+
+            koreaServerList = new List<Tuple<String, String>>();
+            japanServerList = new List<Tuple<String, String>>();
+
+            for (int i = 0; i < tmpList1.Count(); i = i + 2)
+            {
+                koreaServerList.Add(Tuple.Create(tmpList1[i], tmpList1[i + 1]));
+            }
+
+            for (int i = 0; i < tmpList2.Count(); i = i + 2)
+            {
+                japanServerList.Add(Tuple.Create(tmpList2[i], tmpList2[i + 1]));
+            }
+
+            this.ActiveControl = button1;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private Boolean IsIPInRange(String ip, String startRange, String endRange)
+        {
+            int[] ipParts = ip.Split('.').Select(x => Int32.Parse(x)).ToArray();
+            int[] startRangeParts = startRange.Split('.').Select(x => Int32.Parse(x)).ToArray();
+            int[] endRangeParts = endRange.Split('.').Select(x => Int32.Parse(x)).ToArray();
+
+            for (int i = 0; i < ipParts.Count(); i++)
+            {
+                if (ipParts[i] < startRangeParts[i] || ipParts[i] > endRangeParts[i])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Dictionary<IpV4Address, int> IPDict = new Dictionary<IpV4Address, int>();
+
+            using (PacketCommunicator communicator = selectedDevice.Open(1024, PacketDeviceOpenAttributes.Promiscuous, 200))
+            {
+                Packet packet;
+
+                using (BerkeleyPacketFilter filter = communicator.CreateFilter("ip and udp"))
+                {
+                    communicator.SetFilter(filter);
+                }
+                for (int i = 0; i < 10; i++)
+                {
+                    PacketCommunicatorReceiveResult result = communicator.ReceivePacket(out packet);
+                    switch (result)
+                    {
+                        case PacketCommunicatorReceiveResult.Timeout:
+                            continue;
+                        case PacketCommunicatorReceiveResult.Ok:
+                            IpV4Datagram ip = packet.Ethernet.IpV4;
+                            if (ip.Source.ToString() == localIP.ToString())
+                            {
+                                if (IPDict.ContainsKey(ip.Destination))
+                                    IPDict[ip.Destination]++;
+                                else
+                                    IPDict.Add(ip.Destination, 1);
+                            }
+                            else if (ip.Destination.ToString() == localIP.ToString())
+                            {
+                                if (IPDict.ContainsKey(ip.Source))
+                                    IPDict[ip.Source]++;
+                                else
+                                    IPDict.Add(ip.Source, 1);
+                            }
+                            break;
+                        default:
+                            throw new InvalidOperationException();
+                    }
+                }
+            }
+            textBox1.Text = "알 수 없음";
+
+            if (IPDict.Count() == 0)
+            {
+                textBox2.Text = "";
+                return;
+
+            }
+
+            String mostCommonIP = IPDict.Aggregate((a, b) => a.Value > b.Value ? a : b).Key.ToString();
+
+            textBox2.Text = mostCommonIP;
+
+            if (koreaServerList.Any(x => IsIPInRange(mostCommonIP.ToString(), x.Item1, x.Item2)))
+            {
+                textBox1.Text = "한국";
+                return;
+            }
+
+            if (japanServerList.Any(x => IsIPInRange(mostCommonIP.ToString(), x.Item1, x.Item2)))
+            {
+                textBox1.Text = "일본";
+                return;
+            }
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            this.selectedDevice = LivePacketDevice.AllLocalMachine[comboBox1.SelectedIndex];
+            IPHostEntry host = Dns.GetHostEntry(Dns.GetHostName());
+            for (int i = 0; i < host.AddressList.Length; i++)
+            {
+                if (host.AddressList[i].AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork)
+                {
+                    localIP = host.AddressList[i];
+                }
+            }
+        }
+    }
+}
